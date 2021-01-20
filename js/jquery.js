2f3758db23a537093c0d4c8cc118d826c0bc0a86
Ignacio Matteo <ignacio.matteo27@gmail.com>
2021-01-19 21:38:36 -0300

    creacion del repo

---
+++ js/jquery.js
@@ -0,0 +1,12 @@
+$(function() {
+    $( "p" ).text( "Estoy incorporando selectores al proyecto" );
+    });
+
+
+$("p").fadeOut();
+
+$(function() {
+    $('#MiBoton').on('click', function (e) {
+    alert("Funciona!!");
+  });
+});
